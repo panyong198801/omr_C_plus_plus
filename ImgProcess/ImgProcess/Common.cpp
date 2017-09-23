2c6acb5c21fbc2d06dfbd13efad18897fe58682e
panyong <py@mteach.cn>
2017-09-23 11:17:25 +0800

    应用大型考试
     图像处理:cximage opencv
    
    识别流程：
    1. 模板xml加载 ；
    2.科目考号识别，含二维码条码omr；
    3.客观题识别；
    4.主观题切割

---
+++ ImgProcess/ImgProcess/Common.cpp
@@ -0,0 +1,96 @@
+
+#include "stdafx.h"
+#include "Common.h"
+
+
+CCommon::CCommon()
+{
+	bWriteLog = true;
+	m_nTraceMode=0;
+	//m_file = new CFile(); 
+}
+
+
+
+CCommon::~CCommon()
+{
+	if (m_file)
+		m_file.Close();
+}
+
+void CCommon::CreateLogFile(CString strFileName)
+{
+	CTime tNow;
+	//DeleteFile();
+	if(INVALID_HANDLE_VALUE != (HANDLE)m_file.m_hFile)		
+		return;
+	
+	//CFile	m_FileTmp;
+	if(!m_file.Open(strFileName,
+		CFile::modeCreate|CFile::modeReadWrite|CFile::modeNoTruncate|CFile::shareDenyNone,NULL))
+	{
+		m_file.m_hFile = NULL;
+	}
+
+}
+
+
+
+/*---------------------------------------------------------
+函数说明:记录日志信息(时间记录的毫秒)
+输入参数： 
+	strDebugInfo:	需要记录的日志信息
+---------------------------------------------------------*/
+void CCommon::WriteSysFile(CString strDebugInfo,int nTraceMode)
+{
+	if(nTraceMode > m_nTraceMode)	return;
+
+	if(!bWriteLog)
+		return;
+
+	if (m_file.m_hFile == NULL)
+		return;
+
+	SYSTEMTIME systime;	
+	GetLocalTime(&systime);
+//	CTime tNow;
+	CString strLogText;
+
+	if (strDebugInfo == _T(""))
+		return;
+
+//	tNow = CTime::GetCurrentTime();
+	strLogText.Format(_T("%.4u-%.2u-%.2u %.2u:%.2u:%.2u:%.3u	%s \r\n"), systime.wYear, systime.wMonth, systime.wDay,
+				systime.wHour, systime.wMinute, systime.wSecond,systime.wMilliseconds, strDebugInfo);
+
+	if (m_file.m_hFile == NULL || INVALID_HANDLE_VALUE == (HANDLE)m_file.m_hFile)
+	{
+		//JYSH	20120531	当文件对象不存在时，先创建一个临时文件。
+	/*	if(NULL == m_FileTmp.m_hFile || INVALID_HANDLE_VALUE == (HANDLE)m_file.m_hFile)
+		{
+			if(m_FileTmp.Open(L"CommonTmp.txt",CFile::modeCreate|CFile::modeReadWrite|CFile::modeNoTruncate|CFile::shareDenyNone,NULL))
+			{
+				m_FileTmp.SeekToEnd();
+				USES_CONVERSION;
+				m_FileTmp.Write(T2A(strLogText), strlen(T2A(strLogText)));
+			}
+		}*/
+		return;
+	}
+	
+	
+	m_file.SeekToEnd();
+	USES_CONVERSION;
+	m_file.Write(T2A(strLogText), strlen(T2A(strLogText)));
+	//strLogText.ReleaseBuffer ();
+}
+
+
+void CCommon::WriteSysFile(CString strClass,CString strDebugInfo,int nTraceMode)
+{
+	CString	strInfo;
+	strInfo =  strClass + L"[" + strDebugInfo  + L"]" ;
+
+	WriteSysFile(strInfo,nTraceMode);
+}
+
